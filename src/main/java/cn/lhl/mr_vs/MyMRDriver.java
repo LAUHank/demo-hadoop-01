bedc57de58813283e4f573208e0a81a596a318f4
LAUHank <liuhongliang@myhope365.com>
2018-09-13 16:35:01 +0800

    modify driver

--- src/main/java/cn/lhl/mr_vs/MyMRDriver.java
+++ src/main/java/cn/lhl/mr_vs/MyMRDriver.java
@@ -19,22 +19,24 @@ public class MyMRDriver extends Configured implements Tool {
 	@Override
 	public int run(String[] args) throws Exception {
 		Configuration conf = getConf();
-		Job job = Job.getInstance(conf, args[0]);
+		Job job = Job.getInstance(conf);
+		
+		//-Dmapreduce.job.name="jobName"
+		//job.setJobName(args[0]);
 		
 		job.setJarByClass(MyMRDriver.class);
-		
 		job.setMapperClass(MyMapper.class);		
-		
-		job.setSortComparatorClass(MyKeyComparator.class);//二次排序
-		job.setPartitionerClass(MyPartitioner.class);//分区
-		job.setGroupingComparatorClass(MyWritableComparator.class);//分组
-		
 		job.setReducerClass(MyReducer.class);
 		job.setOutputKeyClass(MyKeyWritable.class);
 		job.setOutputValueClass(NullWritable.class);
 		
-		FileInputFormat.addInputPath(job, new Path(args[1]));
-		FileOutputFormat.setOutputPath(job, new Path(args[2]));
+		//-Dmapred.output.key.comparator.class=cn.lhl.mr_vs.MyKeyComparator
+		//job.setSortComparatorClass(MyKeyComparator.class);//二次排序
+		job.setPartitionerClass(MyPartitioner.class);//分区
+		job.setGroupingComparatorClass(MyWritableComparator.class);//分组
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
@@ -42,18 +44,13 @@ public class MyMRDriver extends Configured implements Tool {
 	public static void main(String[] args) {
 		/*
 yarn jar a.jar cn.lhl.mr_vs.MyMRDriver \
--Dmapreduce.job.queuename=oncourse \
-lhl-sort-02 \
-/user/lhl13/valsort \
-/user/lhl13/xiti_out/sort
-
-yarn jar a.jar cn.lhl.mr_vs.MyMRDriver \
+-Dmapred.output.key.comparator.class=cn.lhl.mr_vs.MyKeyComparator \
 -Dmapreduce.job.queuename=oncourse \
 -Dmapred.output.compress=true \
 -Dmapred.output.compression.codec=org.apache.hadoop.io.compress.GzipCodec \
-lhl-sort-02 \
+-Dmapreduce.job.name="mr-auxsort" \
 /user/lhl13/valsort \
-/user/lhl13/xiti_out/sort_gz_4
+/user/lhl13/xiti_out/sort_gz_21
 		 */
 		int status = -1;
 		try {
